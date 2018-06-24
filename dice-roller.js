006a5c4e260a1f6f608fd184554ebea160194f78
Tom Tibs <ttibble90@gmail.com>
2018-06-24 01:58:46 -0500

    Fixed errors with diceroller, added coin flip

--- dice-roller.js
+++ dice-roller.js
@@ -16,18 +16,25 @@ module.exports = {
 
         let diceCount = 1;
         let sides = 6;
-        let args = commandString.split('d');
+        let args = commandString.toLowerCase().split('d');
 
         if (args.length === 1){
+            console.log("sides is now set to " - args[0]);
             diceCount = 1;
             sides = args[0];
         }
         else if (args.length === 2) {
             diceCount = args[0];
             sides = args[1];
+            if(diceCount < 1) diceCount = 1;
         }
         else {return "something went wrong, and this bot is confused...."}
 
         return this.rollDice(diceCount, sides);
+    },
+
+    coinFlip: function () {
+        let flip = Math.ceil(Math.random() * 2);
+        return flip === 1 ? "Heads!!!" : "Tails!!!";
     }
 };
