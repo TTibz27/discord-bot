dd64ef247068e7f06ed7b4a12b1f81b4e779bf42
Tom Tibs <ttibble90@gmail.com>
2019-01-20 08:51:00 -0600

    updated commands, it should now be a single switch statement that handles messages to be parsed

--- bot.js
+++ bot.js
@@ -21,73 +21,67 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 
-    client.user.setUsername("MemeBot 2.0");
+    client.user.setUsername("MemeBot 2.1");
     console.log(`Logged in as ${client.user.tag}!`);
 
 });
 
 client.on('message', msg => {
-    if (msg.content === '!ping') {
-        msg.reply('pong');
-    }
-    if (msg.content === '!marco') {
-        msg.reply('polo!');
-    }
-    if (msg.content === "!dolphin"){
-        memeGenerator.dolphinMeme(msg);
-    }
-
-    else if (msg.content === "!meme"){
-        msg.reply('One repost coming right up!');
-        memeGenerator.randomMeme(msg);
-
-    }
-
-    else if (msg.content.substring(0, 1) === '!') {
+    if (msg.content.substring(0, 1) === '!') {
+        //get the message string
         let message = msg.content;
+        // break it down into an array of words
         let args = message.substring(1).split(' ');
+        //check the first word for see which command it is
         let cmd = args[0].toLowerCase();
 
         logger.debug("COMMAND FOUND");
         logger.debug(cmd);
         logger.debug(args);
 
+        // Find the correct command
         switch(cmd){
             case 'add':
                 memeGenerator.addMemes(msg, args[1]);
                 break;
-
+            case 'meme':
+                msg.reply('One repost coming right up!');
+                memeGenerator.randomMeme(msg);
+                break;
             case 'crivitz':
                 memeGenerator.crivitz(msg);
                 break;
-
             case 'wow':
                 memeGenerator.owenWilsonMeme(msg);
-            break;
-
+                 break;
             case 'dolph':
                 memeGenerator.dolphMeme(msg);
-            break;
-
+                break;
+            case 'dolphin':
+                memeGenerator.dolphinMeme(msg);
+                break;
             case 'touchdown':
                 memeGenerator.oobidooMeme(msg);
                 break;
-
             case 'help':
                 msg.reply('HOW ABOUT YOU HELP URSELF M8');
                 break;
-
             case 'schedule':
-
                 break;
-
+            case 'insult':
+                msg.reply("more insults coming soon, but for now, eat it nerd.");
+                break;
             case 'roll':
                msg.reply(diceRoller.parseDiceCommand(args[1]));
                 break;
-
             case 'flip':
                 msg.reply(diceRoller.coinFlip());
-                break
+                break;
+            case 'marco':
+                msg.reply("polo!");
+                break;
+            case 'ping':
+                msg.reply("pong!");
         }
     }
 });
