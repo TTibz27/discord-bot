17f7a98c756ef2475f55ffa437d7d05578d9d071
RC Cola <41647118+Royal-Crown-Cola@users.noreply.github.com>
2019-09-23 19:02:18 -0600

    added a second file to read, restructured the if else statements. added new conditions for 'denver', 'colorado', and 'memebot'

--- modules/insult-gen/insult-gen.js
+++ modules/insult-gen/insult-gen.js
@@ -1,23 +1,39 @@
-module.exports = function insultGenerator(filename, name, callback) {
-    const fs = require('fs');
-    fs.readFile(filename, function (err, data) {
-        if (err) throw err;
-
-        // Data is a buffer that we need to convert to a string
-        // Improvement: loop over the buffer and stop when the line is reached
-        const lines = data.toString('utf-8').split("\n");
-        const randomNum = [Math.floor(Math.random() * lines.length)]
-        const insult = `Hey ${name}! ${lines[+randomNum]}`;
-        const names = ['Tom', 'Ben', 'Scott', 'Quinn', 'Niel', 'Mike',
-        'Gudac', 'David', 'Alex', 'Carlos', "Cyndi", "Vic"
-    ];
-        if (randomNum > lines.length) {
-            return callback('No insult found you nerd', null);
-        } else if(!name) {
-            let randomName = names[Math.floor(Math.random() * names.length)];
-            name = randomName
-        }
-
-        callback(null, insult);
-    });
-}
+module.exports = insultGenerator(name, callback) {
+    const fs = require("fs");
+    let insult = "";
+            if (!name) {
+                fs.readFile("insult.txt", function (err, data) {
+                    if (err) throw err;
+                    const lines = data.toString("utf-8").split("\n");
+                    const randomNum = [Math.floor(Math.random() * lines.length)];
+                    const names = ["Tom", "Ben", "Scott", "Quinn", "Niel", "Mike", "Gudac", "David", "Alex", "Carlos", "Cyndi", "Vic"];
+                    let randomName = names[Math.floor(Math.random() * names.length)];
+                    name = randomName
+                    insult = `Hey ${name}! ${lines[+randomNum]}`;
+                    callback(null, insult);
+                });
+            } else if (name.toLowerCase() === "denver" || name.toLowerCase() === "colorado") {
+                insult =  `${name} is wonderful. You'll love it here. See you soon. Real soon.`
+                callback(null, insult);
+            } else if (name == "memebot") {
+                fs.readFile("hal.txt", function (err, data) {
+                    if (err) throw err;
+                    const lines = data.toString("utf-8").split("\n");
+                    const randomNum = [Math.floor(Math.random() * lines.length)];
+                    insult = `${lines[+randomNum]}`
+                    callback(null, insult);
+                });
+            }
+            else {
+                fs.readFile("insult.txt", function (err, data) {
+                    if (err) throw err;
+                    const lines = data.toString("utf-8").split("\n");
+                    const randomNum = [Math.floor(Math.random() * lines.length)];
+                    insult = `Hey ${name}! ${lines[+randomNum]}`;
+                    callback(null, insult);
+                });
+            }
+        };
+    // insultGenerator("mike", function (err, line) {
+    //     console.log(line);
+    // });
