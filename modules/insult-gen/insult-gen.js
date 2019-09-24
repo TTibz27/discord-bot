f001c90c02c15c892f9f0d2c85701a7094ff8de2
Tom Tibs <ttibble90@gmail.com>
2019-09-23 21:55:02 -0500

    fixed how module was being imported.

--- modules/insult-gen/insult-gen.js
+++ modules/insult-gen/insult-gen.js
@@ -1,39 +1,38 @@
-module.exports = insultGenerator(name, callback) {
-    const fs = require("fs");
-    let insult = "";
-            if (!name) {
-                fs.readFile("insult.txt", function (err, data) {
-                    if (err) throw err;
-                    const lines = data.toString("utf-8").split("\n");
-                    const randomNum = [Math.floor(Math.random() * lines.length)];
-                    const names = ["Tom", "Ben", "Scott", "Quinn", "Niel", "Mike", "Gudac", "David", "Alex", "Carlos", "Cyndi", "Vic"];
-                    let randomName = names[Math.floor(Math.random() * names.length)];
-                    name = randomName
-                    insult = `Hey ${name}! ${lines[+randomNum]}`;
-                    callback(null, insult);
-                });
-            } else if (name.toLowerCase() === "denver" || name.toLowerCase() === "colorado") {
-                insult =  `${name} is wonderful. You'll love it here. See you soon. Real soon.`
+module.exports = {
+    insult: function (name, callback) {
+        const fs = require("fs");
+        let insult = "";
+        if (!name) {
+            fs.readFile("insult.txt", function (err, data) {
+                if (err) throw err;
+                const lines = data.toString("utf-8").split("\n");
+                const randomNum = [Math.floor(Math.random() * lines.length)];
+                const names = ["Tom", "Ben", "Scott", "Quinn", "Niel", "Mike", "Gudac", "David", "Alex", "Carlos", "Cyndi", "Vic"];
+                name = names[Math.floor(Math.random() * names.length)];
+                insult = `Hey ${name}! ${lines[+randomNum]}`;
                 callback(null, insult);
-            } else if (name == "memebot") {
-                fs.readFile("hal.txt", function (err, data) {
-                    if (err) throw err;
-                    const lines = data.toString("utf-8").split("\n");
-                    const randomNum = [Math.floor(Math.random() * lines.length)];
-                    insult = `${lines[+randomNum]}`
-                    callback(null, insult);
-                });
-            }
-            else {
-                fs.readFile("insult.txt", function (err, data) {
-                    if (err) throw err;
-                    const lines = data.toString("utf-8").split("\n");
-                    const randomNum = [Math.floor(Math.random() * lines.length)];
-                    insult = `Hey ${name}! ${lines[+randomNum]}`;
-                    callback(null, insult);
-                });
-            }
-        };
-    // insultGenerator("mike", function (err, line) {
-    //     console.log(line);
-    // });
+            });
+        } else if (name.toLowerCase() === "denver" || name.toLowerCase() === "colorado") {
+            insult = `${name} is a god damned hellhole. it snows all the time and its hard to breathe there."+
+            "Tom would have left this as a positive comment if he didnt have to go in and fix Ski's code.`;
+            callback(null, insult);
+        } else if (name === "memebot") {
+            fs.readFile("hal.txt", function (err, data) {
+                if (err) throw err;
+                const lines = data.toString("utf-8").split("\n");
+                const randomNum = [Math.floor(Math.random() * lines.length)];
+                insult = `${lines[+randomNum]}`
+                callback(null, insult);
+            });
+        }
+        else {
+            fs.readFile("insult.txt", function (err, data) {
+                if (err) throw err;
+                const lines = data.toString("utf-8").split("\n");
+                const randomNum = [Math.floor(Math.random() * lines.length)];
+                insult = `Hey ${name}! ${lines[+randomNum]}`;
+                callback(null, insult);
+            });
+        }
+    }
+};
