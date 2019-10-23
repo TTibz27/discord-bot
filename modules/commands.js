85cf34be058a1e140894af8a7b2fbb7987242f28
Tom Tibs <ttibble90@gmail.com>
2019-10-23 00:09:22 -0500

    added custom command features such as, averting infinate loops, attaching pictures, and saving commands between sessionwss

--- modules/commands.js
+++ modules/commands.js
@@ -3,12 +3,28 @@ const scheduler = require('./scheduler');
 const diceRoller = require('./dice-roller');
 const memeGenerator = require('./meme-generator');
 const insultGenerator = require('./insult-gen/insult-gen');
+const fs = require('fs');
+const request =require('request');
+const attachmentDir = "images/custom";
+const customCommandFile = "config/commands.txt";
+// Custom Commands will be an array of objects so we can leverage array functions, ie push
+const customCommands = [];
 
 
 // all commands should take in msg then args, regardless of type.
 // it should probably be an interface or something, but this isnt typescript sooooooo.
 
 //(msg, args) =>{...}
+
+function init() {
+ const data = fs.readFileSync(customCommandFile);
+ const cmds = JSON.parse(data);
+ for(let i =0; i < cmds.length; i ++){
+     customCommands.push(cmds[i]);
+     console.log("pushing command");
+ }
+}
+
 const memeCommands = {
     add:(msg, args)=>{ memeGenerator.addMemes(msg, args[1]);},
     meme:(msg)=>{
@@ -39,20 +55,77 @@ const miscCommands = {
     ping:(msg, args)=>{msg.reply('pong.')},
 
 };
-// Custom Commands will be an array of objects so we can leverage array functions, ie push
-const customCommands = [];
 
 function addCustomCommand (msg, args){
-    msg.reply("adding command...")
     console.log(args[1]);
-    customCommands.push({name: args[1].toLowerCase(), message: args.slice(2).join(" ")});
+
+    for (let i = 0; i < customCommands.length; i++) {
+        if (args[1].toLowerCase() === customCommands[i].name) {
+            msg.reply("removing old command...");
+            customCommands.splice(i);
+        }
+    }
+    // This .first() will return undefined if there is no object in the collection...
+    // the collection object is weird in discordjs for some reason, and caches if you try to make an array?
+    // I just want to know if one exists, why do I have to use a special library for that?
+    if(msg.attachments.first()){
+        let url = msg.attachments.first().url.replace('https','http');
+        let filename = attachmentDir +"/"+msg.attachments.first().filename;
+        try{
+            downloadImage(url,filename, function(){
+                msg.reply('Image Command Saved!');
+            });
+            customCommands.push({name: args[1].toLowerCase(), message: args.slice(2).join(" "), file: filename});
+        }
+        catch(err){
+            msg.reply("Error found:  "+err)
+        }
+    }
+    else {
+        if (!args[2]){
+            msg.reply("This command has no text, so it will not be added.")
+        }
+        else{
+            customCommands.push({name: args[1].toLowerCase(), message: args.slice(2).join(" "), file: null});
+            msg.reply("New command added!");
+        }
+    }
+    if (customCommands.length >0){
+        //save array to file
+        fs.writeFileSync(customCommandFile, JSON.stringify(customCommands) , function(err) {
+
+            if(err) {
+                return console.log(err);
+            }
+
+            console.log("The file was saved!");
+        });
+
+    }
+}
+
+// helper function, duplicated in meme-generator, should probably refactor this away eventually.
+function downloadImage (uri, filename, callback){
+
+    request.head(uri, function(err, res, body){
+        console.log('content-type:', res.headers['content-type']);
+        console.log('content-length:', res.headers['content-length']);
+
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
+}
+
+function isEmptyObject(obj) {
+    return !Object.keys(obj).length;
 }
 
 module.exports = {
+    initCustomCommands: init,
     memeCommands: memeCommands,
     diceCommands: diceRollerCommands,
     insultCommands: insultCommands,
     customCommands: customCommands,
     miscCommands: miscCommands,
-    addCustom: addCustomCommand
-}
+    addCustom: addCustomCommand,
+    customAttachmentDir : attachmentDir
+};
